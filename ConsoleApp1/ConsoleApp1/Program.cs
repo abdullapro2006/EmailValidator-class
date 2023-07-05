e61fc895ab4585e5b5501bace478383798ac3044
abdullapro2006 <abdullahmanafli38@gmail.com>
2023-07-05 18:45:14 +0400

    EmailValidator code created

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,79 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace ShoesOrder
+{
+    public abstract class Shoes
+    {
+        public string CompanyName { get; set; }
+        
+        public int Value { get; set; }
+        public override string ToString()
+        {
+            return CompanyName + " " + " " + Value;
+        }
+
+        public Shoes(string companyname,  int value)
+        {
+            CompanyName = companyname;
+            Value = value;
+            
+            
+        }
+        public abstract void TotalValue(int count);
+    }
+    public class TheFisrtShoes : Shoes
+    {
+        public override void TotalValue(int count)
+        {
+            Value = count * 200;
+        }
+        public TheFisrtShoes(string companyname,  int value) : base(companyname, value) { }
+
+
+
+       
+    }
+    public class TheSecondShoes : Shoes
+    {
+        public override void TotalValue(int count)
+        {
+            Value = count * 1000;
+        }
+        public TheSecondShoes(string companyname,int value) : base(companyname,value) 
+        {
+
+        }
+    }
+    public class TheThirdShoes : Shoes
+    {
+        public override void TotalValue(int count)
+        {
+            Value = count * 150;
+        }
+        public TheThirdShoes(string companyname,int value) : base (companyname,value)
+        {
+
+        }
+    }
+    public class Program 
+    { 
+        public static void Main(string[] args)
+        {
+            TheFisrtShoes theFisrtShoes = new TheFisrtShoes("Adidas",  200);
+            Console.WriteLine(theFisrtShoes);
+            TheSecondShoes theSecondShoes = new TheSecondShoes("Gucci",  1000);
+            Console.WriteLine(theSecondShoes);
+            TheThirdShoes theThirdShoes = new TheThirdShoes("Nike", 150);
+            Console.WriteLine(theThirdShoes);
+            theFisrtShoes.TotalValue(4);
+            Console.WriteLine(theFisrtShoes.Value);
+            theSecondShoes.TotalValue(2);
+            Console.WriteLine(theSecondShoes.Value);
+            theThirdShoes.TotalValue(1);
+            Console.WriteLine(theThirdShoes.Value);
+        }
+    }
+
+
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace MyProgram
+{
+    public enum Planets
+        
+    {
+        Mercury=1,
+        Earth=2,
+        Mars=4,
+        Saturn,
+        Jupiter,
+        Plato
+
+
+
+    }
+    public enum PlanetsRadius 
+    {
+        Mercury =2345 ,
+        Earth =1234,
+        Mars =3456,
+        Saturn=4567,
+        Jupiter=5678,
+        Plato=1267,
+    }
+
+
+
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int radius = (int)PlanetsRadius.Mercury;
+            Console.WriteLine(radius);
+        }
+    }
+}
+
